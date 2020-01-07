046e869dcaf14cfc00b0a7042bafa4b6033e21a9
Mweya Ruider <mweyaruider@gmail.com>
2020-01-07 21:54:22 +0100

    Dependency management

---
+++ client/components/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { s3 } from './translations';
+
+export default class Footer extends Component {
+
+    render() {
+
+        return (
+            <View>
+                <TouchableOpacity style={{ alignSelf: 'center' }} onPress={() => navigate('HiringQuestion')}>
+                    <Text style={{ fontSize: 10, color: 'rgba(0,122,255,1)' }}>{s3}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
---
+++ client/components/translations.js
@@ -0,0 +1,18 @@
+// Name arrays what you want, string at first index is lang A, second is lang B ...
+// 0 -> English
+// 1 -> Deutsch
+
+export const s1 = ["Check my code out!", "Siehe Sie meinen Code"]
+export const s2 = ["Settings", "Einstellugen"]
+export const s3 = ["Made in Bavaria", "Mit Späß im Bayern gemacht"]
+export const s4 = ["Check out my CV!", "Lesen Sie meinen Lebenslauf!"]
+export const s5 = ["For more about my education, projects and experiance tap the button below.", "Für mehr über mein Bildung, Projekte und Erfahrung, druck die Knopf hier untern."]
+export const s6 = ["Sorry, something went wrong", "Es tut mir leid, irgendwas habt Schiefgelaufen"]
+export const s7 = ["Are you looking for a new developer?", "Bist du auf der Suche nach einem neuen Entwickler?"]
+export const s8 = ["Yeah!", "Ja!"]
+export const s9 = ["No", "Nein"]
+export const s10 = ["Change color scheme", "Farbe Thema ändern"]
+export const s11 = ["Buy me a drink", "Kauf mir was zu trinken"]
+export const s12 = ["Toggle telemetry", "Telemetrie auß/eincheiten"]
+export const s13 = ["Proxy Settings", "Proxy Einstellugen"]
+export const s14 = ["Not finished building this yet", "Noch nicht fertig"]
