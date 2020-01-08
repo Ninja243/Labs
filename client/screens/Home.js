ab61d33c2e89e17cd8e702ab7b1a1d7fe3330fd5
Mweya Ruider <mweyaruider@gmail.com>
2020-01-08 12:49:17 +0100

    Login/Home page pretty well set

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -25,26 +25,29 @@ export default class HomeScreen extends Component {
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <QuoteBlock />
                     <Button style={{ alignSelf: 'center' }} title="Log in with Auth0 or Google" onPress={this.login} />
-                    <View style={{ height: 95, width: 190, paddingTop: 40, paddingBottom: 10, flexDirection: 'column', justifyContent: 'center', alignContent: "flex-end" }}>
-                        <Text>"I have and agree with the Privacy Policy and Terms of Service" - You</Text>
+                    <View style={{ width: '50%', paddingTop: 40, paddingBottom: 10, flexDirection: 'column', justifyContent: 'center', alignContent: "flex-end" }}>
+                        <Text style={{ fontSize: 15 }}>"I have read and agree with the Privacy Policy and Terms of Service"</Text>
+                        <Text style={{ alignSelf: 'flex-end' }}>-You</Text>
+                        <View style={{ paddingTop: 10, alignSelf: 'flex-start' }}>
+                            <TouchableOpacity>
+                                <Text style={{ color: 'rgba(0, 122, 255, 1)', textDecorationLine: 'underline' }}>Privacy Policy</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Text style={{ color: 'rgba(0, 122, 255, 1)', textDecorationLine: 'underline' }}>Terms of Service</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignSelf: 'flex-start', paddingTop: 10, }}>
+                            <Text style={{ paddingRight: 100 }}>Agreed</Text>
+                            <Switch onValueChange={() => { this.setState({ accepted: false }) }} value={this.state.accepted} thumbColor="rgba(0, 122, 255, 1)" />
+                        </View>
                     </View>
-                    <View>
-                        <TouchableOpacity>
-                            <Text style={{ color: 'rgba(0, 122, 255, 1)', textDecorationLine: 'underline' }}>Privacy Policy</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text style={{ color: 'rgba(0, 122, 255, 1)', textDecorationLine: 'underline' }}>Terms of Service</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <Switch onValueChange={() => { this.setState({ accepted: false }) }} value={this.state.accepted} thumbColor="rgba(0, 122, 255, 1)" />
-
                     <QuoteBlock />
                 </View>
                 :
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <QuoteBlock />
                     <Button style={{ alignSelf: 'center' }} title="Log in with Auth0 or Google" onPress={this.login} disabled />
-                    <View style={{  width: 190, paddingTop: 40, paddingBottom: 10, flexDirection: 'column', justifyContent: 'center', alignContent: "flex-end" }}>
+                    <View style={{ width: '50%', paddingTop: 40, paddingBottom: 10, flexDirection: 'column', justifyContent: 'center', alignContent: "flex-end" }}>
                         <Text>You need to have read and agreed with the Privacy Policy and the Terms of Service to use this app. Tap the links below to read them.</Text>
                         <View style={{ paddingTop: 10, alignSelf: 'flex-start' }}>
                             <TouchableOpacity>
@@ -55,12 +58,12 @@ export default class HomeScreen extends Component {
                             </TouchableOpacity>
                         </View>
                         <View style={{ flexDirection: 'row', alignSelf: 'flex-start', paddingTop: 10, }}>
-                            <Text style={{paddingRight: 100}}>Agreed</Text>
+                            <Text style={{ paddingRight: 100 }}>Agreed</Text>
                             <Switch onValueChange={() => { this.setState({ accepted: true }) }} value={this.state.accepted} />
                         </View>
                     </View>
-                    
-                    
+
+
                     <QuoteBlock />
                 </View>
         );
