917c27c5489ccfd67da64d97d9cec1164093cbb6
Mweya Ruider <mweyaruider@gmail.com>
2020-02-04 13:34:37 +0100

    turns out, my test script is just broken

--- Test scripts/AudienceAPITest.py
+++ Test scripts/AudienceAPITest.py
@@ -13,12 +13,13 @@ response_type = "code"
 nonce = "nonce"
 redirect_uri = "https://auth.expo.io/@mweya/labsclient"
 connection = "google-oauth2"
+scope = "openid profile email"
 
 print("1. Get Token (POST)\n")
 
 payload = "{\"client_id\":\"" + client_id + "\", \"client_secret\":\"" + \
     client_secret + "\", \"audience\":\""+audience + \
-    "\", \"grant_type\":\""+grant_type+"\"}"
+    "\", \"grant_type\":\""+grant_type+"\", \"scope\":\""+scope+"\"}"
 headers = {'content-type': "application/json"}
 print("Request:\n\tHeader\n"+str(headers)+"\n\tPayload\n"+payload)
 conn.request("POST", "/oauth/token", payload, headers)
